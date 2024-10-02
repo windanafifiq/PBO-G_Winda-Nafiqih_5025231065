e28819daf3a66d9e90cba676d32fe1faa1c5dec7
Winda Nafiqih <143302828+windanafifiq@users.noreply.github.com>
2024-10-02 19:43:39 +0700

    Create person.java

---
+++ Auction/person.java
@@ -0,0 +1,21 @@
+/**
+ * Write a description of class Person here.
+ *
+ * @author Winda Nafiqih Irawan
+ * @version October 2, 2024
+ */
+public class Person {
+    private String name;
+    
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String toString() {
+        return name;
+    }
+}
